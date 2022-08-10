67986729e92d79329f6b8770dd62052f24a3e4ea
KhaiVox <khaiduchoa@gmail.com>
2022-08-10 20:43:54 +0700

    soft delete

--- src/app/controllers/CoursesController.js
+++ src/app/controllers/CoursesController.js
@@ -22,10 +22,8 @@ class CoursesController {
 
     // [POST] /courses/store
     store(req, res, next) {
-        const formData = req.body
-        // link ảnh kiểu của youtube
-        formData.image = `https://img.youtube.com/vi/${req.body.videoId}/sddefault.jpg`
-        const course = new Course(formData)
+        req.body.image = `https://img.youtube.com/vi/${req.body.videoId}/sddefault.jpg`
+        const course = new Course(req.body)
         course
             .save()
             // sau khi lưu thành công sẽ quay về trang chúng ta thiết lập bên dưới
@@ -54,7 +52,22 @@ class CoursesController {
 
     // [DELETE] /courses/:id
     destroy(req, res, next) {
+        // Course.delete là phương thức xóa mềm của thư viện "Mongoose Delete Plugin"
+        Course.delete({ _id: req.params.id })
+            .then(() => res.redirect('back'))
+            .catch(next)
+    }
+
+    // [DELETE] /courses/:id/force
+    forceDestroy(req, res, next) {
         Course.deleteOne({ _id: req.params.id })
+            .then(() => res.redirect('back'))
+            .catch(next)
+    }
+
+    // [RESTORE] /courses/:id/restore
+    restore(req, res, next) {
+        Course.restore({ _id: req.params.id })
             .then(() => res.redirect('back'))
             .catch(next)
     }
